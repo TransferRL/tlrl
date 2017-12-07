058917d37e90dcc112c8fa04d70aed1eb61c8399
Feng <chifeng1113@gmail.com>
2017-12-07 16:22:49 -0500

    changed env dictionary

--- ammar_TrRBM/envs.py
+++ ammar_TrRBM/envs.py
@@ -3,6 +3,7 @@ from lib.env.cartpole import CartPoleEnv
 from lib.env.threedcartpole import ThreeDCartPoleEnv
 from lib.env.mountain_car import MountainCarEnv
 from lib.env.acrobot import AcrobotEnv
+from lib.env.atari_env import AtariEnv
 
 ENVS_DICTIONARY = {
     '3DMountainCar': ThreeDMountainCarEnv
@@ -13,10 +14,12 @@ ENVS_DICTIONARY = {
 }
 #
 ENVS_PATH_DICTIONARY = {
-    '3DMountainCar': {'env': ThreeDMountainCarEnv, 'instances_path': '../data/3d_mountain_car/'}
-    , '2DMountainCar': {'env': MountainCarEnv, 'instances_path': '../data/2d_mountain_car/'}
-    , '2DCartPole': {'env': CartPoleEnv, 'instances_path': '../data/2d_cart_pole/'}
-    , '3DCartPole': {'env': ThreeDCartPoleEnv, 'instances_path': '../data/3d_cart_pole/'}
-    , 'Acrobot': {'env': AcrobotEnv, 'instances_path': '../data/acrobot/'}
+    '3DMountainCar': {'env': ThreeDMountainCarEnv(), 'instances_path': '../data/3d_mountain_car/'}
+    , '2DMountainCar': {'env': MountainCarEnv(), 'instances_path': '../data/2d_mountain_car/'}
+    , '2DCartPole': {'env': CartPoleEnv(), 'instances_path': '../data/2d_cart_pole/'}
+    , '3DCartPole': {'env': ThreeDCartPoleEnv(), 'instances_path': '../data/3d_cart_pole/'}
+    , 'Acrobot': {'env': AcrobotEnv(), 'instances_path': '../data/acrobot/'}
+    , 'Pong': {'env': AtariEnv(game='pong', obs_type='ram'), 'instances_path': '../data/Pong/'}
+    , 'breakout': {'env': AtariEnv(game='breakout', obs_type='ram'), 'instances_path': '../data/breakout/'}
 }
 
--- ammar_TrRBM/trrbm.py
+++ ammar_TrRBM/trrbm.py
@@ -147,18 +147,24 @@ class RBM(object):
         self.tf_session.run(init)
 
         pbar = tqdm(range(self.num_epochs))
+        counter = 0
+
         for i in pbar:
             avg_err = self.one_train_step(v1_input, v2_input)
             self.cost.append(avg_err)
             pbar.set_description('squared reconstruction average batch error: {}'.format(avg_err))
 
             # catch divergence
-            if np.isnan(self.cost[-1]) == True:
+            if np.isnan(self.cost[-1]):
                 raise RuntimeError('Training has diverged - lower learning rate!')
             # early stopping
-            if i > 20 and np.mean(self.cost[-40:-20]) - np.mean(self.cost[-20:]) < np.mean(self.cost[-40:]) * 0.005:
+            if i > 1 and np.mean(self.cost[-10:-5]) - np.mean(self.cost[-5:]) < np.mean(self.cost[-10:]) * 0.005:
+                counter += 1
+                if counter >= 5:
+                    self.learning_rate = self.learning_rate / 2
+                    counter = 0
+            if self.learning_rate < 1e-10:
                 break
-
         return self.cost
 
     def one_train_step(self, v1_input, v2_input):
